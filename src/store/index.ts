19ebd2ae7996102956535d66818d494f4405be58
qianchen <qianchen@upchina.com>
2021-07-27 10:15:39 +0800

    vuex

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,10 +5,21 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    count: 0,
+  },
+  getters: {
+
   },
   mutations: {
+    increment(state) {
+      state.count+=Math.floor(Math.random()*10);
+    },
+    decrement(state) {
+      state.count--;
+    },
   },
   actions: {
+    
   },
   modules: {
   }
--- src/views/About/index.vue
+++ src/views/About/index.vue
@@ -1,5 +1,26 @@
 <template>
   <div class="about">
-    <h1>This</h1>
+    <button @click="decrement">-</button>
+    <h1>{{count}}</h1>
+    <button @click="increment">+</button>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue';
+import {mapState} from 'vuex';
+export default Vue.extend({
+  name:'About',
+  computed: {
+    ...mapState(['count']),
+  },
+  methods: {
+    decrement(){
+      this.$store.commit('decrement');
+    },
+    increment(){
+      this.$store.commit('increment');
+    },
+  }
+})
+</script>
